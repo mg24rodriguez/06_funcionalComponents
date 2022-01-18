238b11c52a15f981ba6f1ec14399dfb4f508d0f2
mg24rodriguez <mg24rodriguez@gmail.com>
2022-01-17 19:33:36 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.png';
+import './App.css';
+import PersonalCard from './components/PersonalCard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1> Ejercicio todo junto - React </h1>
+      </header>
+
+        <PersonalCard firstName = {"Mario"} lastName = {"Rodriguez"} age = {36} haircolor = {"Rubio rizado"} />
+        <PersonalCard firstName = {"Silvi"} lastName = {"Moncayo"} age = {34} haircolor = {"Castaño ondulado"} />
+        <PersonalCard firstName = {"Gabriel"} lastName = {"Rodriguez"} age = {2} haircolor = {"Castaño ondulado"} />
+        <PersonalCard firstName = {"Nidia"} lastName = {"Benavides"} age = {57} haircolor = {"Castaño rizado"} />
+        
+    </div>
+  );
+}
+
+export default App;
